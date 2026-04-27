ed089998846e7582f1b0300c4b6723c0fde80991
rodriguezgeorge089-art <rodriguezgeorge089@gmail.com>
2026-04-27 18:28:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
